a11fdffe70bf9d2537d6a22110769d25f4e2fbce
Ali Ismayilov <ali@ismailov.info>
2014-02-21 22:03:27 -0300

    user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,30 @@
 class User < ActiveRecord::Base
+  attr_accessor :recently_created
+
+  validates :email, presence: true, uniqueness: true
+  validates :uid, presence: true, uniqueness: true
+  validates :access_token, presence: true, uniqueness: true
+
+  after_create do
+    self.recently_created = true
+  end
+
+  def recently_created?
+    @recently_created
+  end
+
+  def self.find_or_create_with_omniauth(auth)
+    user = self.find_or_create_by provider: auth["provider"], uid: auth["uid"]
+
+    user.assign_attributes({
+      name:             auth["info"]["name"],
+      email:            auth["info"]["email"],
+      avatar_photo_url: auth["info"]["image"],
+      access_token:     auth["credentials"]["token"],
+    })
+
+    user.save!
+
+    user
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,85 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe :validations do
+    it { should validate_presence_of    :email }
+    it { should validate_uniqueness_of  :email }
+
+    it { should validate_presence_of    :uid }
+    it { should validate_uniqueness_of  :uid }
+
+    it { should validate_presence_of    :access_token }
+    it { should validate_uniqueness_of  :access_token }
+  end
+
+  describe '.find_or_create_with_omniauth' do
+    let(:auth) do
+      {
+        provider: 'facebook', uid: '123456',
+        credentials: { token: 'AAADv' },
+        extra: {}, info: {
+          email: 'ali@ismailov.info',
+          name: 'Ali Ismayilov'
+        }
+      }.with_indifferent_access
+    end
+
+    context 'user does not exists' do
+      describe 'creating a new user' do
+        it 'creates a new user' do
+          expect {
+            new_user = User.find_or_create_with_omniauth(auth)
+
+            expect(new_user).to be_recently_created
+
+          }.to change(User, :count).by(1)
+        end
+      end
+
+      describe 'setting data on new user' do
+        subject { User.find_or_create_with_omniauth(auth) }
+
+        its(:access_token)  { should eql 'AAADv'                    }
+        its(:provider)      { should eql 'facebook'                 }
+        its(:uid)           { should eql '123456'                   }
+        its(:name)          { should eql 'Ali Ismayilov'            }
+        its(:email)         { should eql 'ali@ismailov.info'        }
+      end
+    end
+
+    context 'user already exists' do
+      let!(:existing_user) do
+        create(:user, provider: 'facebook', uid: '123456')
+      end
+
+      describe 'finding existing user' do
+        it 'finds existing user' do
+          expect(User.find_or_create_with_omniauth(auth)).to eql(existing_user)
+        end
+
+        it 'does not create a user' do
+          expect {
+            old_user = User.find_or_create_with_omniauth(auth)
+
+            expect(old_user).to_not be_recently_created
+
+          }.to_not change(User, :count)
+        end
+      end
+
+      describe 'updating existing user' do
+        before do
+          User.find_or_create_with_omniauth(auth)
+        end
+
+        subject { existing_user.reload }
+
+        its(:access_token)  { should eql 'AAADv'                    }
+        its(:provider)      { should eql 'facebook'                 }
+        its(:uid)           { should eql '123456'                   }
+        its(:name)          { should eql 'Ali Ismayilov'            }
+        its(:email)         { should eql 'ali@ismailov.info'        }
+      end
+    end
+  end
 end
