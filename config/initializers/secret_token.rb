a7c1fa20fbbad4ba491a50e566abf7027d0d8835
Ali Ismayilov <ali@ismailov.info>
2014-02-21 21:22:56 -0300

    Initial commit.
    Generated by pah version 0.0.12

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meclis::Application.config.secret_key_base = '6e8efe65556100c5c49640023e116fbf5b34b1db0b26cf0dd7328b5027a8bdd6afff72edc84d13e5c376fa48963ab1e7ab935ba9255f50104d0276e2947da780'
