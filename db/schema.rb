33c46c7f0b6d465980ebb862f080ea301bd570db
Ali Ismayilov <ali@ismailov.info>
2014-02-22 19:07:21 -0300

    add votable_type to votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140222215527) do
+ActiveRecord::Schema.define(version: 20140222220627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140222215527) do
     t.integer  "votable_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "votable_type"
   end
 
   add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
