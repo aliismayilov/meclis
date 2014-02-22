49fb960c237cf564c0c7ac3b81e5fb74280bd84b
Ali Ismayilov <ali@ismailov.info>
2014-02-21 21:24:03 -0300

    Replace secret token with environment variable.
    Generated by pah version 0.0.12

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Meclis::Application.config.secret_key_base = '6e8efe65556100c5c49640023e116fbf5b34b1db0b26cf0dd7328b5027a8bdd6afff72edc84d13e5c376fa48963ab1e7ab935ba9255f50104d0276e2947da780'
+Meclis::Application.config.secret_key_base = ENV['SECRET_TOKEN']
