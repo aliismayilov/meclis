30b7a14f75d2acdc2f1398432d84783b912d0d7c
Ali Ismayilov <ali@ismailov.info>
2014-02-23 03:17:55 -0300

    application contoller filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,32 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   ensure_security_headers # See more: https://github.com/twitter/secureheaders
 
+  before_filter :save_return_path
+  helper_method :current_user, :user_signed_in?
+
+  protected
+  def save_return_path
+    session[:return_path] = request.path
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    session.delete(:user_id)
+    nil
+  end
+
+  def user_signed_in?
+    current_user.present?
+  end
+
+  def authenticate!
+    return if user_signed_in?
+
+    origin = request.post? ? root_url : request.fullpath
+
+    redirect_to(login_url(provider: 'facebook', origin: origin),
+                notice: t('sessions.notice'))
+  end
+
 end
